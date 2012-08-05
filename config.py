76db5a4191659fe8039eec4497bbc4c917b0f5b1
fred6 <frexids@gmail.com>
2012-08-05 03:16:11 -0400

    add slash to blather rule

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 site_title = 'fred6'
-site_dir = 'site/'
+site_dir = 'site'
 out_dir = 'out'
-templates_dir = 'templates/'
+templates_dir = 'templates'
 pages_ext = '.md'
 page_format = 'markdown'
 
@@ -10,7 +10,7 @@ templates['default_layout'] = 'layout'
 templates['default_page'] = 'page_default'
 
 render_rules = {
-    'blather/*': {'page_template': 'page_blather'},
+    '/blather/*': {'page_template': 'page_blather'},
     '/index': {'page_template': 'index_content'}
 }
 
