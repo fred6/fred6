c9f6a8034cf9ebbe8ea4de673c80bfb63003d2f0
fred6 <frexids@gmail.com>
2012-08-12 15:46:35 -0400

    make template specifying a first-class part of site rules

--- config.py
+++ config.py
@@ -9,8 +9,9 @@ templates = {}
 templates['default_layout'] = 'layout'
 templates['default_page'] = 'page_default'
 
-r = [('/blather/*', 'page_router', {'page_template': 'page_blather'}),
+
+r = [('/blather/*', 'page_router', 'page_blather'),
      ('/notes/*', 'page_router'),
-     ('/', 'index_router', {'page_template': 'index_content'}),
+     ('/', 'index_router', 'index_content'),
      ('*.md', 'page_router'),
      ('*.*', 'id_router')]
